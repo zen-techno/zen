7e3668ee6ade2f86d8e549b0ebfbb0f7d95383cf
Rusih100 <rusih100@gmail.com>
2023-10-10 23:37:33 +1000

    Костыль для автоматического создания суперпользователя

--- alembic/versions/ebeb6ca67de9_init_database.py
+++ alembic/versions/ebeb6ca67de9_init_database.py
@@ -7,9 +7,9 @@ Create Date: 2023-10-10 21:56:21.960740
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'ebeb6ca67de9'
